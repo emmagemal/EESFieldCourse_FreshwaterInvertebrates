fed261db504c9a3443347efe097f1954d373aea3
sophierose841 <s1739832@ed.ac.uk>
2020-09-14 13:53:21 +0100

    final graph code

--- waterq_distance.R
+++ waterq_distance.R
@@ -7,10 +7,29 @@
 #loading ggplot
 library(ggplot2)
 
+#edit:taking from alldata
+#loading all data
+alldata <- read.csv("Data/alldata.csv")
+
+#waterq/distance with alldata
+
+ggplot(alldata, aes(x = distance_source_km, y = ASPT)) +
+  theme_light() +
+  labs(
+    x = "Direct Distance from Source (m)",
+    y = "ASPT Water Quality",
+    title = "Water Quality with Increasing Distance from Source of Braid Burn",
+    subtitle = "Water quality according to ASPT index."
+  ) +
+  geom_point() +
+  geom_text(aes(label = alldata$site_number, hjust=1.3, vjust=1))
+
+#--old code for graphs--
+
 #reading new data set for creating graph
 data1 <- read.csv("Data/graph_data.csv")
 
-#graph waterq/distance
+#graph waterq/distance and land use
 ggplot(data1, aes(x = Dist_source, y = ASPT)) +
   theme_light() +
   labs(
@@ -21,7 +40,23 @@ ggplot(data1, aes(x = Dist_source, y = ASPT)) +
        ) +
   geom_point(aes(shape = factor(Land_use)))
 
-data2 <-                
+#same graph but no land use
+ggplot(data1, aes(x = Dist_source, y = ASPT)) +
+  theme_light() +
+  labs(
+    x = "Direct Distance from Source (m)",
+    y = "ASPT Water Quality",
+    title = "Water Quality with Increasing Distance from Source of Braid Burn",
+    subtitle = "Water quality according to ASPT index."
+  ) +
+  geom_point() +
+  geom_text(aes(label = data1$Site_no, hjust=1.3, vjust=1))
 
+
+#reading data for spearmans
+data2 <- read.csv("Data/alldata.csv")               
+
+#spearman water q/distance
+cor.test(data2$distance_source_km, data2$ASPT)
 
 

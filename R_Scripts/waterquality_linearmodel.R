7bd8398130d698dd9c4ef91f3b7320c67288e799
emmagemal <emmagemal@gmail.com>
2020-09-29 14:07:17 +0100

    Emma changed model process to get same result

--- R_Scripts/waterquality_linearmodel.R
+++ R_Scripts/waterquality_linearmodel.R
@@ -49,120 +49,110 @@ ggcorr(cm_data, size = 3, hjust = 0.9, label = TRUE, layout.exp = 2)
 ggsave(file = "correlation_matrix.png", width = 5, height = 5)
 
 # nitrate is the most correlated parameter with ASPT (R = -0.6)
+# other correlated parameters (>|0.3|) = conductivity, depth, phosphate, velocity, BOD
 # color has no effect on ASPT (R = 0)
 # absorbance, turbidity, sediment size, and width have basically no effect on ASPT (R = ±0.1)
 
+# conductivity very correlated with nitrate (0.6), will probably only use 1 of the 2
+# depth and pH fairly correlated (0.5), width and phosphate fairly correlated (0.5)
+
 
 ## Making a linear model for WQ ----
+# comparison of main correlated parameters (nitrate, phosphate, conductivity, depth)
+main1 <- lm(ASPT ~ avg.nitrate, data = imp_data)   
+
+main2 <- lm(ASPT ~ avg.phosphate, data = imp_data) 
+
+main3 <- lm(ASPT ~ avg.conductivity_uScm, data = imp_data) 
+
+main4 <- lm(ASPT ~ avg.depth_cm, data = imp_data) 
+
+AIC(main1, main2, main3, main4)
+# nitrate is the best, do not remove this from the model 
+
+# not including parameters with ≤0.2 correlation with ASPT
+# elimination of bottom parameter each time
 m1 <- lm(ASPT ~ avg.nitrate 
          + avg.phosphate
          + avg.conductivity_uScm
          + avg.pH
          + avg.velocity_ms
          + avg.depth_cm
-         + avg.colour_mgLPt
-         + avg.turbidity_FTU
-         + avg.sediment_cm
-         + BODmgL
-         + avg.width_cm
-         + avg.absorbance, data = imp_data)  
+         + BODmgL, data = imp_data)  
 
 m2 <- lm(ASPT ~ avg.nitrate 
          + avg.phosphate
          + avg.conductivity_uScm
          + avg.pH
          + avg.velocity_ms
-         + avg.depth_cm
-         + avg.colour_mgLPt
-         + avg.turbidity_FTU
-         + avg.sediment_cm
-         + BODmgL
-         + avg.width_cm, data = imp_data) 
+         + avg.depth_cm, data = imp_data)  
 
 m3 <- lm(ASPT ~ avg.nitrate 
          + avg.phosphate
          + avg.conductivity_uScm
          + avg.pH
-         + avg.velocity_ms
-         + avg.depth_cm
-         + avg.colour_mgLPt
-         + avg.turbidity_FTU
-         + avg.sediment_cm
-         + BODmgL, data = imp_data) 
+         + avg.velocity_ms, data = imp_data)  
 
 m4 <- lm(ASPT ~ avg.nitrate 
          + avg.phosphate
          + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms
-         + avg.depth_cm
-         + avg.colour_mgLPt
-         + avg.turbidity_FTU
-         + avg.sediment_cm, data = imp_data) 
+         + avg.pH, data = imp_data)  
 
 m5 <- lm(ASPT ~ avg.nitrate 
          + avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms
-         + avg.depth_cm
-         + avg.colour_mgLPt
-         + avg.turbidity_FTU, data = imp_data) 
+         + avg.conductivity_uScm, data = imp_data)  
 
 m6 <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms
-         + avg.depth_cm
-         + avg.colour_mgLPt, data = imp_data) 
+         + avg.phosphate, data = imp_data)  
 
-m7 <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms
-         + avg.depth_cm, data = imp_data) 
-
-m8 <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms, data = imp_data) 
-
-m9 <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH, data = imp_data) 
-
-m10 <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.conductivity_uScm, data = imp_data) 
-
-m11 <- lm(ASPT ~ avg.nitrate 
-          + avg.phosphate, data = imp_data) 
-
-m12 <- lm(ASPT ~ avg.nitrate, data = imp_data) 
 
 # comparing the F-values of the models
-AIC(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12)
+AIC(m1, m2, m3, m4, m5, m6) 
+# best is m3
 
-# since conductivity and NO3- are somewhat correlated (R = 0.6), 
-# testing the best model (lowest F value using AIC) without conductivity
-m8x <- lm(ASPT ~ avg.nitrate 
-         + avg.phosphate
-         + avg.pH
+# removal of conductivity from best model thus far
+m3_cond <- lm(ASPT ~ avg.nitrate 
+           + avg.phosphate
+           + avg.pH
+           + avg.velocity_ms, data = imp_data) 
+
+AIC(m3, m3_cond)
+# removing conductivity made it better 
+
+# elimination of top parameter each time (always keeping nitrate), based on m3_cond
+m7 <- lm(ASPT ~ avg.nitrate 
+          + avg.pH
+          + avg.velocity_ms, data = imp_data)  
+
+m8 <- lm(ASPT ~ avg.nitrate 
+          + avg.velocity_ms, data = imp_data)  
+
+# other possible logical combinations
+m9 <- lm(ASPT ~ avg.nitrate 
+          + avg.pH, data = imp_data)  
+
+m10 <- lm(ASPT ~ avg.nitrate 
+         + avg.depth_cm   # since it's quite correlated to pH
+         + avg.width_cm   # since it's quite correlated to phosphate
          + avg.velocity_ms, data = imp_data) 
 
-# testing to see if nitrate should be removed instead of conductivity 
-m8y <- lm(ASPT ~ avg.phosphate
-         + avg.conductivity_uScm
-         + avg.pH
-         + avg.velocity_ms, data = imp_data) 
+m11 <- lm(ASPT ~ avg.nitrate 
+          + avg.pH  
+          + avg.width_cm   # since it's quite correlated to phosphate
+          + avg.velocity_ms, data = imp_data) 
 
-AIC(m8, m8x, m8y)   # m8x is the best (only <1.0 difference, but it's simpler)
+m12 <- lm(ASPT ~ avg.nitrate 
+          + avg.depth_cm   # since it's quite correlated to pH
+          + avg.phosphate   
+          + avg.velocity_ms, data = imp_data) 
 
-summary(m8x)
+# comparing the F-values of the models
+AIC(m3, m3_cond, m7, m8, m9, m10, m11, m12)
+# depth and width did not improve the model, made it worse, so sticking to pH and phosphate
+# m3_cond still the best by almost a value of 2
+
+
+summary(m3_cond)
 # intercept = 12.7565
 # nitrate slope = -3.1772 
 # phosphate slope = -5.3183
@@ -173,47 +163,20 @@ summary(m8x)
 
 
 # testing another model 
-wq_model <- lm(ASPT ~ ASPT 
+wq_model <- lm(ASPT ~ 1 
                + avg.nitrate 
                + avg.phosphate
                + avg.pH
                + avg.velocity_ms, data = imp_data) 
+summary(wq_model)
 
-test <- lm(distance_source_km ~ ASPT 
-                       + avg.nitrate 
-                       + avg.phosphate
-                       + avg.pH
-                       + avg.velocity_ms, data = imp_data) 
+nulltest <- lm(ASPT ~
+               + avg.nitrate 
+               + avg.phosphate
+               + avg.pH
+               + avg.velocity_ms, data = imp_data) 
+summary(nulltest)
 
-lm.predict <- cbind(imp_data, predict(test, interval = 'confidence'))
-
-(wq_plot2 <- ggplot(wq_data, aes(x = distance_source_km, y = WQ)) +
-                labs(
-                  x = "Distance from Source (km)",
-                  y = "ASPT"
-                ) +
-                stat_smooth(method = "lm", color = "black", alpha = 0.7) +
-                geom_point(size = 7.5) +
-                theme_pubr() +
-                theme(axis.text = element_text(size = 36),
-                      axis.title = element_text(size = 40),
-                      axis.title.x = element_text(margin = margin(t = 20)),
-                      axis.title.y = element_text(margin = margin(r = 20)),
-                      plot.margin=unit(c(1,1,1,1),"cm")))
-
-(wq_testplot <- ggplot(lm.predict, aes(x = distance_source_km, y = ASPT)) +
-                  labs(
-                    x = "Distance from Source (km)",
-                    y = "ASPT"
-                  ) +
-                  geom_point(size = 3) +
-                  geom_line() +
-                  theme_pubr() +
-                  theme(axis.text = element_text(size = 10),
-                        axis.title = element_text(size = 15),
-                        axis.title.x = element_text(margin = margin(t = 2)),
-                        axis.title.y = element_text(margin = margin(r = 2)),
-                        plot.margin=unit(c(1,1,1,1),"cm")))
                       
 
 

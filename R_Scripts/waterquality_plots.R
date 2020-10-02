3f12dbdd02a41c5bf0f7cdcf07527f188b2d7331
emmagemal <emmagemal@gmail.com>
2020-10-02 10:43:09 +0100

    Emma modified some plots

--- R_Scripts/waterquality_plots.R
+++ R_Scripts/waterquality_plots.R
@@ -98,57 +98,7 @@ ggsave(wq_plot, file = "ASPTdist_plot.png", width = 14, height = 14, units = c("
                                                   "Urban",
                                                   "Grassland")))
 
-ggsave(landuse_plot, file = "ASPTdist_landuse.png", width = 7, height = 5.5, units = c("in"), path = "Figures")
-
-
-## Investigation of impacts of outlier ----
-wq_less <- wq_data[-c(8), ]   # removing a potential outlier to see effects 
-
-less_stats <- lm(ASPT ~ distance_source_km,
-                 data = wq_less)
-summary(less_stats)
-# p-value = 0.04944 (still significant, but barely!!)
-# F-value = 4.694 (1 and 13 DF)
-# R-squared = 0.2653
-
-(plot_less <- ggplot(wq_less, aes(x = distance_source_km, y = ASPT,
-                                  color = landuse_type)) +
-                  labs(
-                    x = "Distance from Source (km)",
-                    y = "Average Score Per Taxon",
-                    color = "Land Use Type",
-                    shape = "Land Use Type") +
-                  stat_smooth(method = "lm", color = "black", alpha = 0.7) +
-                  geom_point(size = 3.5, aes(shape = landuse_type, stroke = 1.2)) +
-                  theme_pubr() +
-                  theme(axis.text = element_text(size = 10),
-                        axis.text.x = element_text(margin = margin(t = 0)),
-                        axis.text.y = element_text(margin = margin(r = 0)),
-                        axis.title = element_text(size = 15),
-                        axis.title.x = element_text(margin = margin(t = 10)),
-                        axis.title.y = element_text(margin = margin(r = 10)),
-                        axis.ticks = element_blank(),
-                        legend.position = "right",
-                        plot.margin=unit(c(1,1,1,1),"cm")) +
-                  scale_color_jama(labels = c("Natural Woodland",
-                                              "Managed Woodland",
-                                              "Parkland",
-                                              "Urban",
-                                              "Grassland")) +
-                  scale_shape_manual(values = c(18, 15, 16, 17, 8),
-                                     labels = c("Natural Woodland",
-                                                "Managed Woodland",
-                                                "Parkland",
-                                                "Urban",
-                                                "Grassland")) +
-                  expand_limits(y = c(4, 8.1)))   # to show direct comparison with all data            
-
-ggsave(plot_less, file = "ASPTdist_nooutlier.png", width = 7, height = 5.5, units = c("in"), path = "Figures")
-
-# plotting both land use plots on one panel
-landuse_grid <- ggarrange(landuse_plot, plot_less, nrow = 1, common.legend = TRUE, legend = "top") 
-ggsave(landuse_grid, file = "landuseplots.png", width = 12, height = 5.5, units = c("in"), path = "Figures")
-
+ggsave(landuse_plot, file = "ASPTdist_landuse.png", width = 8, height = 5.5, units = c("in"), path = "Figures")
 
 
 
@@ -177,6 +127,40 @@ summary(BBWI_stats)
                         plot.margin=unit(c(1,1,1,1),"cm")))
 
 ggsave(file = "BBWIdist_plot.png", width = 14, height = 14, units = c("in"), path = "Figures")
+
+# creating plot of BBWI vs distance, with land use as a variable 
+(landuse_plot2 <- ggplot(wq_data, aes(x = distance_source_km, y = WQ,
+                                      color = landuse_type)) +
+                    labs(
+                      x = "Distance from Source (km)",
+                      y = "Braid Burn Water Quality Index",
+                      color = "Land Use Type",
+                      shape = "Land Use Type") +
+                    stat_smooth(method = "lm", color = "black", alpha = 0.7) +
+                    geom_point(size = 3.5, aes(shape = landuse_type, stroke = 1.2)) +
+                    theme_pubr() +
+                    theme(axis.text = element_text(size = 10),
+                          axis.text.x = element_text(margin = margin(t = 0)),
+                          axis.text.y = element_text(margin = margin(r = 0)),
+                          axis.title = element_text(size = 15),
+                          axis.title.x = element_text(margin = margin(t = 10)),
+                          axis.title.y = element_text(margin = margin(r = 10)),
+                          axis.ticks = element_blank(),
+                          legend.position = "right",
+                          plot.margin=unit(c(1,1,1,1),"cm")) +
+                    scale_color_jama(labels = c("Natural Woodland",
+                                                "Managed Woodland",
+                                                "Parkland",
+                                                "Urban",
+                                                "Grassland")) +
+                    scale_shape_manual(values = c(18, 15, 16, 17, 8),
+                                       labels = c("Natural Woodland",
+                                                  "Managed Woodland",
+                                                  "Parkland",
+                                                  "Urban",
+                                                  "Grassland")))
+
+ggsave(landuse_plot2, file = "BBWIdist_landuse.png", width = 8, height = 5.5, units = c("in"), path = "Figures")
 
 
 
@@ -209,3 +193,53 @@ ggsave(file = "ASPT_BBWI_plot.png", width = 14, height = 14, units = c("in"), pa
 # making a panel of BBWI vs distance and ASPT vs BBWI 
 otherfigs <- ggarrange(bbwi_plot, wq_aspt_plot, nrow = 1) 
 ggsave(otherfigs, file = "bbwi_aspt_distancepanel.png", width = 24, height = 11, units = c("in"), path = "Figures")
+
+
+
+## Investigation of impacts of outlier ----
+wq_less <- wq_data[-c(8), ]   # removing a potential outlier to see effects 
+
+less_stats <- lm(ASPT ~ distance_source_km,
+                 data = wq_less)
+summary(less_stats)
+# p-value = 0.04944 (still significant, but barely!!)
+# F-value = 4.694 (1 and 13 DF)
+# R-squared = 0.2653
+
+(plot_less <- ggplot(wq_less, aes(x = distance_source_km, y = ASPT,
+                                  color = landuse_type)) +
+    labs(
+      x = "Distance from Source (km)",
+      y = "Average Score Per Taxon",
+      color = "Land Use Type",
+      shape = "Land Use Type") +
+    stat_smooth(method = "lm", color = "black", alpha = 0.7) +
+    geom_point(size = 3.5, aes(shape = landuse_type, stroke = 1.2)) +
+    theme_pubr() +
+    theme(axis.text = element_text(size = 10),
+          axis.text.x = element_text(margin = margin(t = 0)),
+          axis.text.y = element_text(margin = margin(r = 0)),
+          axis.title = element_text(size = 15),
+          axis.title.x = element_text(margin = margin(t = 10)),
+          axis.title.y = element_text(margin = margin(r = 10)),
+          axis.ticks = element_blank(),
+          legend.position = "right",
+          plot.margin=unit(c(1,1,1,1),"cm")) +
+    scale_color_jama(labels = c("Natural Woodland",
+                                "Managed Woodland",
+                                "Parkland",
+                                "Urban",
+                                "Grassland")) +
+    scale_shape_manual(values = c(18, 15, 16, 17, 8),
+                       labels = c("Natural Woodland",
+                                  "Managed Woodland",
+                                  "Parkland",
+                                  "Urban",
+                                  "Grassland")) +
+    expand_limits(y = c(4, 8.1)))   # to show direct comparison with all data            
+
+ggsave(plot_less, file = "ASPTdist_nooutlier.png", width = 7, height = 5.5, units = c("in"), path = "Figures")
+
+# plotting both land use plots (ASPT & no outlier) on one panel
+landuse_grid <- ggarrange(landuse_plot, plot_less, nrow = 1, common.legend = TRUE, legend = "top") 
+ggsave(landuse_grid, file = "landuseplots.png", width = 12, height = 5.5, units = c("in"), path = "Figures")
